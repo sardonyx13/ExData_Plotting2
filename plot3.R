64f4c30c1d65501e78eceab71596de8bc76519d1
Alex Solomin <solomin@gmail.com>
2014-07-23 12:53:56 +0400

    plot 6

--- plot3.R
+++ plot3.R
@@ -1,7 +1,7 @@
 
 NEI<- readRDS("summarySCC_PM25.rds")
 
-#png("plot3.png", width = 480, height = 480)
+png("plot3.png", width = 480, height = 480)
 
 baltimore <- subset(NEI, fips == "24510", select = c(Emissions, year, type))
 
@@ -9,4 +9,4 @@ te <- tapply(baltimore$Emissions, baltimore$year, sum, na.rm = TRUE)
 barplot(te, ylab = "Total Emissions in tons", main = "Total Emissions per Year in Baltimore")
 abline(h = te["2008"], lty = 2)
 
-#dev.off()
+dev.off()
--- plot6.R
+++ plot6.R
@@ -1,6 +1,6 @@
 NEI<- readRDS("summarySCC_PM25.rds")
 
-#png("plot6.png", width = 480, height = 480)
+png("plot6.png", width = 680, height = 680)
 
 baltimore <- subset(NEI, fips == "24510", select = c(Emissions, year, type))
 la <- subset(NEI, fips == "06037", select = c(Emissions, year, type))
@@ -14,8 +14,32 @@ la <- subset(la, type == "ON-ROAD", select = c(Emissions, year))
 tebl <- tapply(baltimore$Emissions, baltimore$year, sum, na.rm = TRUE)
 tela <- tapply(la$Emissions, la$year, sum, na.rm = TRUE)
 
-barplot(tebl, ylab = "Emissions from motor vehicle in tons")
-barplot(tela)
+par(mfcol = c(2,2))
+
+barplot(rbind(tela, tebl),
+        col = c("grey", "white"),
+        main = "Total emissions from motor vehicle per year", 
+        ylab = "Emissions from motor vehicle in tons",
+        legend = c("Los Angeles County", "Baltimore City"),
+        args.legend = list(x = "bottomright"))
+
+barplot(rbind(diff(tela), diff(tebl)), beside = TRUE,
+        col = c("grey", "white"),
+        main = "Changes in emissions between years", 
+        ylab = "Changes in tons",
+        names = c("1999-2002", "2002-2005", "2005-2008"),
+        legend = c("Los Angeles County", "Baltimore City"),
+        args.legend = list(x = "topright"))
 
 
-#dev.off()
+barplot(rbind(diff(tela)/tela[1:3] * 100, diff(tebl)/tebl[1:3] *100), 
+        beside = TRUE,
+        col = c("grey", "white"),
+        main = "Changes in emissions between years", 
+        ylab = "Changes in percent",
+        names = c("1999-2002", "2002-2005", "2005-2008"),
+        legend = c("Los Angeles County", "Baltimore City"),
+        args.legend = list(x = "bottomright"))
+
+
+dev.off()
